9652b9f8cc0959aaea00ad6168ba4b3be38461d9
Bennor McCarthy <bennor.mccarthy@akqa.com>
2023-08-02 13:16:00 +1000

    fix: return response from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,5 +2,8 @@ import { NextResponse } from "next/server";
 
 export default function middleware() {
     const response =  NextResponse.next();
+    response.headers.delete("cache-control");
     response.headers.set("cache-control", "private, max-age=300")
+    response.headers.set("x-cache-control-debug", "private, max-age=300");
+    return response
 }
